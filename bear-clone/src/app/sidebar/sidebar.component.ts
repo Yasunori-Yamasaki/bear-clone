d3b484dfb38870afade952fed156e6795969e41b
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-12 05:35:25 +0000

    feat: #2 サイドバーの構成要素を実装。

--- bear-clone/src/app/sidebar/sidebar.component.ts
+++ bear-clone/src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import {
+  faCalendar,
+  faFolder,
+  faNoteSticky,
+  faSquareCheck,
+  faTrashCan,
+} from "@fortawesome/free-regular-svg-icons";
+import { faAngleDown, faAngleRight, faHashtag, faLock } from "@fortawesome/free-solid-svg-icons";
+import { faAngular } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
-  selector: 'app-sidebar',
+  selector: "app-sidebar",
   standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html'
+  imports: [FontAwesomeModule],
+  templateUrl: "./sidebar.component.html",
 })
 export class SidebarComponent {
-
+  faAngleDown = faAngleDown;
+  faAngleRight = faAngleRight;
+  faAngular = faAngular;
+  faCalendar = faCalendar;
+  faFolder = faFolder;
+  faHashtag = faHashtag;
+  faLock = faLock;
+  faNoteSticky = faNoteSticky;
+  faSquareCheck = faSquareCheck;
+  faTrashCan = faTrashCan;
 }
