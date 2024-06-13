33e1860abb48e586133ca594ce4ad8b0aeb128c0
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-13 10:22:57 +0000

    feat: #3 メモ一覧をベタ書きからデータに変更。

--- bear-clone/src/app/note-list/note-list.component.ts
+++ bear-clone/src/app/note-list/note-list.component.ts
@@ -1,16 +1,42 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CommonModule } from "@angular/common";
+
+type Note = {
+  title: string;
+  content: string;
+  updatedAt: string;
+};
 
 @Component({
   selector: "app-note-list",
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: "./note-list.component.html",
 })
 export class NoteListComponent {
-  faMagnifyingGlass = faMagnifyingGlass;
-  faPenToSquare = faPenToSquare;
+  protected faMagnifyingGlass = faMagnifyingGlass;
+  protected faPenToSquare = faPenToSquare;
+
+  selectedNote: Note | null = null;
+
+  notes: Note[] = [
+    {
+      title: "テストタイトル",
+      content: "テスト本文 これはテストですが、テストじゃない......",
+      updatedAt: "Just now",
+    },
+    {
+      title: "テストタイトル2",
+      content: "テスト本文2 これは本番かと思いきや、テストだ......",
+      updatedAt: "Jun 10",
+    },
+  ];
+
+  selectNote(note: Note): void {
+    this.selectedNote = note;
+  }
 }
