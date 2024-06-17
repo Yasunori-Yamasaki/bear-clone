6fb6bfe1717849ff64c9e1cdd939872dbc742967
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-17 02:48:09 +0000

    feat: #3 エディタ画面用コンポーネントを作成。

--- bear-clone/src/app/app.component.ts
+++ bear-clone/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { NoteListComponent } from "./note-list/note-list.component";
+import { EditorComponent } from "./editor/editor.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NoteListComponent],
+  imports: [RouterOutlet, SidebarComponent, NoteListComponent, EditorComponent],
   templateUrl: "./app.component.html",
   host: {
     class: "flex",
---
+++ bear-clone/src/app/editor/editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './editor.component.html'
+})
+export class EditorComponent {
+
+}
