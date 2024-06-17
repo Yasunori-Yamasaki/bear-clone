1ef29b2056bb4035df9e57d6a3f2da9bee87fc9f
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-17 06:50:12 +0000

    feat: #4 エディタ要素を実装。

--- bear-clone/src/app/editor/editor.component.ts
+++ bear-clone/src/app/editor/editor.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { QuillModule, QuillModules } from "ngx-quill";
 
 @Component({
-  selector: 'app-editor',
+  selector: "app-editor",
   standalone: true,
-  imports: [],
-  templateUrl: './editor.component.html'
+  imports: [QuillModule],
+  templateUrl: "./editor.component.html",
 })
 export class EditorComponent {
-
+  protected modules: QuillModules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["bold", "italic", "underline", "strike"],
+        ["link", "image"],
+        ["blockquote", "code-block"],
+      ],
+    },
+  };
+  protected placeholder = "";
 }
