85a138df5300d45e344b7768be2edfee9a801f25
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-18 12:11:52 +0000

    refactor: #9 メモデータの更新処理をエディタコンポーネントへ移行。

--- bear-clone/src/app/app.component.ts
+++ bear-clone/src/app/app.component.ts
@@ -4,10 +4,6 @@ import { SidebarComponent } from "./sidebar/sidebar.component";
 import { NoteListComponent } from "./note-list/note-list.component";
 import { EditorComponent } from "./editor/editor.component";
 import { Note } from "./shared/models/note.model";
-import { ContentChange } from "ngx-quill";
-import { Store } from "@ngrx/store";
-import { NoteActions } from "./shared/actions/note.actions";
-import dayjs from "dayjs";
 
 @Component({
   selector: "app-root",
@@ -20,25 +16,4 @@ import dayjs from "dayjs";
 })
 export class AppComponent {
   protected selectedNote = signal<Note | null>(null);
-
-  constructor(private store: Store) {}
-
-  contentSave(event: ContentChange): void {
-    const note = this.selectedNote();
-    if (!note) return;
-
-    const newNote: Note = this.setNewNote(note, event);
-
-    this.store.dispatch(NoteActions.updateNotes({ newNote }));
-  }
-
-  setNewNote(oldNote: Note, { html, text }: ContentChange): Note {
-    return {
-      ...oldNote,
-      title: text.split("\n")[0],
-      content: text.split("\n").slice(1).join(""),
-      htmlText: html ?? "",
-      updatedAt: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-    };
-  }
 }
--- bear-clone/src/app/editor/editor.component.ts
+++ bear-clone/src/app/editor/editor.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, output } from "@angular/core";
 import { ContentChange, QuillModule, QuillModules } from "ngx-quill";
 import { Note } from "../shared/models/note.model";
 import { FormsModule } from "@angular/forms";
+import dayjs from "dayjs";
+import { Store } from "@ngrx/store";
+import { NoteActions } from "../shared/actions/note.actions";
 
 @Component({
   selector: "app-editor",
@@ -19,4 +22,34 @@ export class EditorComponent {
   protected modules: QuillModules = {
     toolbar: "#toolbar",
   };
+
+  constructor(private store: Store) {}
+
+  /**
+   * ローカルストレージ内の該当メモデータを更新
+   * @param event リッチエディタのコンテンツ内容変更イベント
+   */
+  update(event: ContentChange): void {
+    if (!this.note) return;
+
+    const newNote = this.setNewNote(this.note, event);
+    this.store.dispatch(NoteActions.updateNotes({ newNote }));
+  }
+
+  /**
+   * 内容更新後のメモデータ成形処理
+   *
+   * @param oldNote 旧メモデータ
+   * @param -{html, text} リッチエディタのコンテンツ内容変更イベント
+   * @returns 内容更新後のメモデータ
+   */
+  setNewNote(oldNote: Note, { html, text }: ContentChange): Note {
+    return {
+      ...oldNote,
+      title: text.split("\n")[0],
+      content: text.split("\n").slice(1).join(""),
+      htmlText: html ?? "",
+      updatedAt: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+    };
+  }
 }
