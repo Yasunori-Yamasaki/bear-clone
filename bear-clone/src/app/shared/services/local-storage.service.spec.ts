9af12609846890f7432a0fe285942f046058c801
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-19 03:17:41 +0000

    test: #9 テストケースを実装。

--- bear-clone/src/app/shared/services/local-storage.service.spec.ts
+++ bear-clone/src/app/shared/services/local-storage.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { LocalStorageService } from './local-storage.service';
+import { LocalStorageService } from "./local-storage.service";
 
-describe('LocalStorageService', () => {
+describe("LocalStorageService", () => {
   let service: LocalStorageService;
 
   beforeEach(() => {
@@ -10,7 +10,17 @@ describe('LocalStorageService', () => {
     service = TestBed.inject(LocalStorageService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  describe("save", () => {
+    it("ローカルストレージにメモデータを保存している。", () => {});
+  });
+
+  describe("get", () => {
+    it("メモデータが保存されていれば、そのデータを返却する。", () => {});
+
+    it("メモデータが保存されていなければ、から配列を返却する。", () => {});
   });
 });
