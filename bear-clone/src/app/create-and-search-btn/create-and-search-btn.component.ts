23a816c2c4b82461eda662ad2ce751d4a6e4a65f
Yasunori-Yamasaki <yamasaki.yasunori@suzukishouten.co.jp>
2024-06-20 10:27:02 +0000

    feat: #8 カテゴリを切り替えると、メモ一覧のカテゴリ名も連動して切り替わるように処理を追加。

--- bear-clone/src/app/create-and-search-btn/create-and-search-btn.component.ts
+++ bear-clone/src/app/create-and-search-btn/create-and-search-btn.component.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component, output } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { CategoryService } from "../shared/services/category.service";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: "app-create-and-search-btn",
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FontAwesomeModule],
+  imports: [AsyncPipe, FontAwesomeModule],
   templateUrl: "./create-and-search-btn.component.html",
   host: {
     class: "ticky top-0 flex h-12 items-center justify-between bg-white p-1",
@@ -19,4 +21,6 @@ export class CreateAndSearchBtnComponent {
 
   protected faMagnifyingGlass = faMagnifyingGlass;
   protected faPenToSquare = faPenToSquare;
+
+  constructor(protected categoryService: CategoryService) {}
 }
